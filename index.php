21480156aab04b740601121754c270ac04474ff7
Slava Samsonov <slavasamsonow@yandex.ru>
2020-04-16 02:03:33 +0400

    Центральный файл системы

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Подгружаем роутер
+use app\core\Router;
+
+// Функция, подгружающая классы
+spl_autoload_register(function($class){
+    $path = str_replace('\\','/', $class.'.php');
+    if(file_exists($path)){
+        require $path;
+    }
+});
+
+function debug($str){
+    echo '<pre>';
+    var_dump($str);
+    echo '</pre>';
+//    exit();
+}
+
+// Открываем сессию
+session_start();
+
+
+$router = new Router;
+$router->run();
+?>
