6c346fc979c61ac7faca1e9fb920463d633aa963
Slava Samsonov <slavasamsonow@yandex.ru>
2020-04-23 02:18:06 +0400

    Подключение статичного jQuery  своих скриптов

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -10,12 +10,15 @@
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title><?= $seo['title'] ?></title>
+
+    <script src="/public/js/jquery-3.5.0.min.js"></script>
+    <script src="/public/js/main.js"></script>
 </head>
 <body>
 
 <header>
     <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
-        <?php if ($this->path == 'Main/index'): ?>
+        <?php if ($_SERVER['REQUEST_URI'] == '/'): ?>
             <span class="navbar-brand">MTF</span>
         <? else: ?>
             <a class="navbar-brand" href="/">MTF</a>
@@ -54,10 +57,6 @@
 </footer>
 
 <!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
         crossorigin="anonymous"></script>
