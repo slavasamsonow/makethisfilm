84b0f9931ea3196e759641d2ac4a3d27eafa2b22
Slava Samsonov <slavasamsonow@yandex.ru>
2020-04-23 02:19:03 +0400

    Функции: страница ошибки, вывод в консоль, перенаправление и вывод данных (для Ajax)

--- app/core/View.php
+++ app/core/View.php
@@ -31,4 +31,36 @@ class View{
         require 'app/views/layouts/'.$this->layout.'.php';
     }
 
+    public static function errorCode($code)
+    {
+        if($code == 403 || $code == 404){
+            http_response_code($code);
+        }
+        $seo = [
+            'title' => 'Ошибка '.$code.' Make This Film',
+        ];
+        ob_start();
+        require 'app/views/errors/'.$code.'.php';
+        $content = ob_get_clean();
+        require 'app/views/layouts/default.php';
+
+        exit();
+    }
+
+    public function toConsole($data){
+        exit(json_encode(['console' => $data]));
+    }
+
+    public function location($url){
+        exit(json_encode(['url' => $url]));
+    }
+
+    public function locationOut($url){
+        exit(json_encode(['urlo' => $url]));
+    }
+
+    public function data($data){
+        exit(json_encode(['data' => $data]));
+    }
+
 }
